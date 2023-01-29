0105491288abcd603d91cbb9adf2d8fabd099898
Your Name <you@example.com>
2023-01-29 15:51:46 +0100

    forward instead of copying

--- redirect_private_channel.py
+++ redirect_private_channel.py
@@ -18,13 +18,14 @@ with open("last_message_id.json", "r+") as f:
     for message in client.iter_messages(PRIVATE_CHAT_ID, offset_id=s["id"], reverse=True):
         print("message id", message.id)
         last_id = message.id
-        if message.message:
-            text = message.message
-            client.send_message(REDIRECT_CHAT_ID, message=text)
-        if message.photo:
-            filepath = client.download_media(message)
-            client.send_file(REDIRECT_CHAT_ID, filepath)
-            os.remove(filepath)
+        message.forward_to(REDIRECT_CHAT_ID)
+        # if message.message:
+        #     text = message.message
+        #     client.send_message(REDIRECT_CHAT_ID, message=text)
+        # if message.photo:
+        #     filepath = client.download_media(message)
+        #     client.send_file(REDIRECT_CHAT_ID, filepath)
+        #     os.remove(filepath)
     print("last id", last_id)
 with open("last_message_id.json", "w+") as f:
     dump({"id": last_id}, f)
